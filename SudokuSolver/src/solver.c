f0a52b24b6b39d9cdfc6cc3d2ac60aed596e4e8d
Goncalo Rodrigues <rodriguesgonalo604@gmail.com>
2019-05-28 09:23:31 +0100

    Changes in functions

--- SudokuSolver/src/solver.c
+++ SudokuSolver/src/solver.c
@@ -1,22 +1,22 @@
 #include "solver.h"
 
-#include "utils/collections.h"
-
-#include "structs.h"
-
 void Solve(unsigned char* pMatrix, GeneralTreeNode* pHistoricalTree)
 {
 	if (!pMatrix) return;
 
 	Vector2I position;
 	List* pPossibilities;
+	int* pNumber;
+	int found = 0;
 
 	// Init possibilities list
 	pPossibilities = CreateList();
 
 	for (int i = 1; i <= 9; ++i)
 	{
-		int* pNumber = (int*)malloc(sizeof(int));
+		pNumber = (int*)malloc(sizeof(int));
+
+		if (!pNumber) return;
 
 		*pNumber = i;
 
@@ -28,23 +28,26 @@ void Solve(unsigned char* pMatrix, GeneralTreeNode* pHistoricalTree)
 	{
 		for (int j = 0; j < 9; ++j)
 		{
-			if (pMatrix[i * 9 + j] != 0)
+			if (pMatrix[i * 9 + j] == 0)
 			{
 				position.x = j;
 				position.y = i;
 
+				found = 1;
+
 				break;
 			}
 		}
+
+		if (found) break;
 	}
 
 	// Apply rules
-
 	LineRule(pMatrix, pPossibilities, position);
 
-	// TODO: Column rule
+	ColumnRule(pMatrix, pPossibilities, position);
 
-	// TODO: Square rule
+	SquareRule(pMatrix, pPossibilities, position);
 }
 
 void LineRule(unsigned char* pMatrix, List* pPossibilities, Vector2I position)
@@ -53,5 +56,34 @@ void LineRule(unsigned char* pMatrix, List* pPossibilities, Vector2I position)
 	if (!pPossibilities) return;
 	if (!pPossibilities->pHead) return;
 
+	List* pExisting = CreateList();
+	int* pNumber;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		if (pMatrix[position.y * 9 + i] != 0)
+		{
+			pNumber = (int*)malloc(sizeof(int));
+
+			if (!pNumber) return;
+
+			*pNumber = pMatrix[position.y * 9 + i];
+
+			ListAddTail(pExisting, (void*)pNumber);
+		}
+	}
+
+	// TODO: Remove
+
+	FreeList(pExisting, free);
+}
+
+void ColumnRule(unsigned char* pMatrix, List* pPossibilities, Vector2I position)
+{
+	// TODO: This function
+}
+
+void SquareRule(unsigned char* pMatrix, List* pPossibilities, Vector2I position)
+{
 	// TODO: This function
 }
--- SudokuSolver/src/solver.h
+++ SudokuSolver/src/solver.h
@@ -1,8 +1,18 @@
 #ifndef SOLVER_H_
 #define SOLVER_H_
 
+#include <stdlib.h>
+
+#include "utils/collections.h"
+
+#include "structs.h"
+
 void Solve(unsigned char* pMatrix, GeneralTreeNode* pHistoricalTree);
 
 void LineRule(unsigned char* pMatrix, List* pPossibilities, Vector2I position);
+
+void ColumnRule(unsigned char* pMatrix, List* pPossibilities, Vector2I position);
+
+void SquareRule(unsigned char* pMatrix, List* pPossibilities, Vector2I position);
 
 #endif // !SOLVER_H_
